27764d275967d391e49e689361b6100dd622f96c
KJPaTyRU <denaminer@gmail.com>
2019-06-17 20:13:30 +1000

    Creating models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+class LessonAdmin(admin.ModelAdmin):
+    exclude = ('date',)
+
+class CategManagerAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(models.Lesson,LessonAdmin)
+admin.site.register(models.CategManager, CategManagerAdmin)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class ABS_Lesson(models.Model):
+    title = None
+
+    def __str__(self):
+        return self.title
+
+class Lesson(ABS_Lesson):
+    title = models.CharField(max_length=255)
+    date = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    #link = models.CharField(max_length=255)
+    # author = models.One
+
+
+class CategManager(ABS_Lesson):
+    title = models.CharField(max_length=255)
+    #sub_lessons = models.ManyToOneRel()
